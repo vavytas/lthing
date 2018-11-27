e1a3470493830c804b69338bfd4d064f1b13b6b9
Vytas <vytas.vadapolas@ktu.edu>
2018-11-27 13:43:05 +0200

    updated passport and other stuff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
                     <passport-clients></passport-clients>
                     <passport-authorized-clients></passport-authorized-clients>
                     <passport-personal-access-tokens></passport-personal-access-tokens>
-                   
+                    Yo
                     
                 </div>
             </div>
